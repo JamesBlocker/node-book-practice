1a275d7d28063aadb4f23b198ca94105c348b1a9
Byron <jamesblocker81@gmail.com>
2018-03-14 08:50:19 -0700

    finish up controller

--- controllers/jokes.js
+++ controllers/jokes.js
@@ -19,7 +19,25 @@ module.exports = {
 
     create: (req, res) => {
         Book.create(req.body, (err, newBook) => {
-            res.direct("books")
+            res.direct("/books")
         })
     },
+
+    edit: (req, res) => {
+        Book.findById(req.params.id, (err, thatBook) => {
+            res.render('books/edit', {book: thatBook })
+        })
+    },
+
+    update: (req, res) => {
+        Book.findByIdAndUpdate(req.params.id, req.body, (err, updatedBook) => {
+            res.redirect(`/books/${req.params.id}`)
+        })
+    },
+
+    destroy: (req, res) => {
+        Book.findByIdAndRemove(req.params.id, (err => {
+            res.redirect("/books")
+        }))
+    }
 }
